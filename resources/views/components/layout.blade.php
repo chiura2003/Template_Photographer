6af4847d9411a8f2b59c46551ef226aac5f83118
chiura2003 <143214114+chiura2003@users.noreply.github.com>
2026-09-16 19:02:15 +0200

    Update page title fallback text

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Portfolio fotografico di Tommaso Fiorini">
-    <title>{{ $title ?? 'Fiorini Tommaso' }}</title>
+    <title>{{ $title ?? 'Nome Fotografo' }}</title>
     <link rel="icon" type="image/png" href="{{ asset('images/tommaso-fiorini-favicon.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('images/tommaso-fiorini-favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
